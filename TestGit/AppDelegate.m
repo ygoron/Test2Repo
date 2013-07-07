90874e7c4143a94bde151864f24a9df3e492476d
ygoron <ygoron@gmail.com>
2013-07-07 13:07:48 -0400

    Test

--- TestGit/AppDelegate.m
+++ TestGit/AppDelegate.m
@@ -13,6 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     //qq
+    //qq2
     // Override point for customization after application launch.
     return YES;
 }
